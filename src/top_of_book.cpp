fdac7feb90a7f9815828a5c8765095ef8a6d02d6
Richard Hickling <richard.t.hickling@gmail.com>
2022-11-19 12:29:07 +0100

    Tidying up

--- src/top_of_book.cpp
+++ src/top_of_book.cpp
@@ -36,16 +36,16 @@ int main(int argc, char* argv[])
 {
     using namespace profitview;
     ProgramArgs options;
-    auto const result = profitview::parseProgramOptions(argc, argv, options);
+    auto const result = parseProgramOptions(argc, argv, options);
     if (result)
         return result.value();
 
     for (auto& file : options.parquetFile)
     {
-        profitview::print_ns::print("Reading {} at one\n", file);
+        print_ns::print("Reading {} at one\n", file);
 
         if (!std::filesystem::exists(file))
-            throw std::runtime_error(profitview::fmt_ns::format("Enable to find file {}", file));
+            throw std::runtime_error(fmt_ns::format("Enable to find file {}", file));
 
         std::shared_ptr<arrow::io::ReadableFile> infile;
         PARQUET_ASSIGN_OR_THROW(infile, arrow::io::ReadableFile::Open(file, arrow::default_memory_pool()));
@@ -54,14 +54,14 @@ int main(int argc, char* argv[])
             parquet::arrow::OpenFile(infile, arrow::default_memory_pool(), &reader));
         std::shared_ptr<arrow::Table> table;
         PARQUET_THROW_NOT_OK(reader->ReadTable(&table));
-        std::cout << "Loaded " << table->num_rows() << " rows in " << table->num_columns()
-                    << " columns." << std::endl;
+
+        fmt_ns::print("Loaded {} rows in {} columns.\n", table->num_rows(), table->num_columns());
 
         const auto& schema{table->schema()};
-        profitview::fmt_ns::print("Field names: \n");
+        fmt_ns::print("Field names: \n");
         for (int i = 0; i < schema->num_fields(); ++i) {
             const auto& field{schema->field(i)};
-            profitview::fmt_ns::print("Field {}: '{}' has type {}\n", i + 1, field->name(), field->type()->name());
+            fmt_ns::print("Field {}: '{}' has type {}\n", i + 1, field->name(), field->type()->name());
         }
     }
 
