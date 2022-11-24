60ccad92e763ec8a7846df02e610b4b52b1f2e47
Richard Hickling <richard.t.hickling@gmail.com>
2022-11-24 22:18:12 +0100

    Removing commented code

--- src/simple_pybind11_test.cpp
+++ src/simple_pybind11_test.cpp
@@ -1,43 +1,4 @@
-// #include "print.hpp"
-// #include "program_options.hpp"
-
 #include <pybind11/pybind11.h>
-
-// #include <boost/program_options.hpp>
-// #include <boost/range/irange.hpp>
-
-// #include <exception>
-// #include <filesystem>
-// #include <vector>
-// #include <memory>
-
-// namespace profitview
-// {
-
-// struct ProgramArgs
-// {
-//     std::vector<int> integers_to_add;
-
-//     void addOptions(boost::program_options::options_description& options)
-//     {
-//         namespace po = boost::program_options;
-//         // clang-format off 
-//         options.add_options()
-//             ("integer_to_add", po::value(&integers_to_add)->multitoken()->required(), "Integers to add together.");
-//         // clang-format on
-//     }
-// };
-
-// }    // namespace profitview
-
-// int add(auto const& integers_to_add) {
-//     int result = 0;
-//     for (auto const& i : integers_to_add)
-//     {
-//         result+=i;
-//     }
-//     return result;
-// }
 
 int add(int i, int j) {
     return i + j;
